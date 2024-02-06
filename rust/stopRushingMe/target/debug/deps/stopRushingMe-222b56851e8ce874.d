732cfc4692436be0b44e74eddbfdfc94a81bcb64
Jayden Ferguson <jayden@Jaydens-MacBook-Pro.local>
2024-02-05 20:21:30 -0600

    rust test program

---
+++ rust/stopRushingMe/target/debug/deps/stopRushingMe-222b56851e8ce874.d
@@ -0,0 +1,5 @@
+/Users/jayden/Code/projects/rust/stopRushingMe/target/debug/deps/stopRushingMe-222b56851e8ce874: src/main.rs
+
+/Users/jayden/Code/projects/rust/stopRushingMe/target/debug/deps/stopRushingMe-222b56851e8ce874.d: src/main.rs
+
+src/main.rs:
---
+++ rust/stopRushingMe/target/debug/stopRushingMe.d
@@ -0,0 +1,1 @@
+/Users/jayden/Code/projects/rust/stopRushingMe/target/debug/stopRushingMe: /Users/jayden/Code/projects/rust/stopRushingMe/src/main.rs
