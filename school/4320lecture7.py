783eb222d3a5ade676b47fc90ea3e778410520fb
Jayden Ferguson <jayden@Jaydens-MacBook-Pro.local>
2024-02-08 11:08:11 -0600

    missed files

---
+++ school/4320lecture7.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib.figure import Figure 
+from matplotlit.backends.backend.backend_agg import FigureCanvasAgg
+
+
