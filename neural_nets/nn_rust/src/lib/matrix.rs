72bc9194e98535c6fa44a047f5438152f3263412
Jayden Ferguson <jayden@Jaydens-MacBook-Pro.local>
2024-02-14 01:02:53 -0600

    Neural Network: Rust

---
+++ neural_nets/nn_rust/src/lib/matrix.rs
@@ -0,0 +1,127 @@
+use rand::{thread_rng, Rng};
+
+
+#[derive(Clone)]
+pub struct Matrix {
+    pub rows: usize,
+    pub cols: usize,   
+    pub data: Vec<Vec<f64>>,
+}
+
+impl Matrix {
+    
+    // functions to create a matrix 
+    pub fn zeros(rows: usize, cols: usize) -> Matrix{
+        Matrix{
+            rows,
+            cols,
+            data: vec![vec![0.0; cols]; rows],
+        }
+    }
+    
+    pub fun random(rows:usize, cols: usize) -> Matrix{
+        let mut rng = thread_rng();
+
+        let mut res = Matrix::zeros(rows, cols);
+    
+        for i in 0..rows {
+            for j in 0.. cols {
+                res.data[i][j] = rng.gen::<f64>() * 2.0 - 1.0;
+            }
+        }  
+
+        res 
+    }
+
+
+    pub fn from(data: Vec<Vec<f64>>) -> Matrix {
+        Matrix {
+            rows: data.len(),
+            cols: data[0].len(),
+            data,
+        }
+    }
+
+    // Matrix multiplication 
+
+    pub fn multiple(&mut self, other: &Matrix) -> Matrix {
+        if self.cols != other.rows {
+            panic!("Attempted multiplication of matrix of incorrect dimensions");
+        }
+
+        let mut res = Matrix::zeros(self.rows, other.cols);
+
+        for i in 0..self.rows {
+            for j in 0..other.cols{
+                let mut sum = 0.0;
+                for k in 0..self.cols{
+                    sum += selfdata[i][k] * other.data[k][j]
+                }
+
+                res.data[i][j] = sum
+            }
+        }
+
+        res 
+    }
+
+    pub fn add(&mut self, other: &Matrix) -> Matrix {
+        if self.rows != other.rows || self.cols != other.cols {
+            panic!("Attempted to add matrix of incorrect dimensions")
+        }
+
+        let mut res = Matrix::zeros(self.rows, self.cols);
+        for i in 0..self.rows {
+            for j in 0..self.cols{
+                res.data[i][j] = self.data[i][j] + other.data[i][j]
+            }
+        }
+
+        res
+    }
+    
+    pub fn dot_product(&mut self, other: &Matrix) -> Matrix { 
+        if self.rows != other.rows || self.cols != other.cols {
+            panic!("Attempted to apply dot dot product matrix of incorrect dimensions")
+        }
+
+        let mut res = Matrix::zeros(self.rows, self.cols);
+        for i in 0..self.rows {
+            for j in 0..self.cols{
+                res.data[i][j] = self.data[i][j] * other.data[i][j]
+            }
+        }
+
+        res
+    }
+
+    pub fn subtract(&mut self, other: &Matrix) -> Matrix {
+        if self.rows != other.rows || self.cols != other.cols {
+            panic!("Attempted to subtract matrix of incorrect dimensions")
+        }
+
+        let mut res = Matrix::zeros(self.rows, self.cols);
+        for i in 0..self.rows {
+            for j in 0..self.cols{
+                res.data[i][j] = self.data[i][j] - other.data[i][j]
+            }
+        }
+
+        res
+    }
+
+    pub fn map(&mut self, function: &dyn FN(f64) -> f64) -> Matrix {
+        // apply every individual element to a function 
+        // put result back into the matrix 
+        Matrix::from((self.data).clone().into_iter()).map(|row| row.into_iter().map(|value| function(value)).collect())        
+    }
+
+    pub fn transpose(&mut self) -> Matrix {
+        let mut res = Matrix::zeros(self.cols, self.rows);
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                res.data[j][i] = self.data[i][j];
+            }
+        }
+    }
+}
---
+++ neural_nets/nn_rust/src/lib/network.rs
@@ -0,0 +1,6 @@
+pub struct Network {
+    layers: Vec<usize>,
+    weights: Vec<Matrix>,
+    biases: Vec<Matrix>,
+    data: Vec<Matrix>
+}
